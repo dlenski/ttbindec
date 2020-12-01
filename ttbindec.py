a26078443119ec795c032b9371233fc1dc04a899
Daniel Lenski <dlenski@amazon.com>
2020-11-30 19:22:20 -0800

    handle larger-than-anticipated records

--- ttbindec.py
+++ ttbindec.py
@@ -18,7 +18,9 @@ def read_header(buf, offset=0, tag2struct=defs.tag2struct):
 
     # sanity checks
     assert tag2struct[tag] == defs.FILE_HEADER
-    assert fh.file_version == 7
+    # Firmware version header changes after version 9:
+    #   https://github.com/ryanbinns/ttwatch/blob/260aff5869fd577d788d86b546399353d9ff72c1/src/ttbin.c#L354
+    assert 7 <= fh.file_version <= 9
 
     offset += 1 + ctypes.sizeof(defs.FILE_HEADER)
     rls = []
@@ -31,19 +33,29 @@ def read_header(buf, offset=0, tag2struct=defs.tag2struct):
         struct = tag2struct[rl.tag]
 
         # check that our expected size of this structure matches what the RECORD_LENGTH says
-        assert ctypes.sizeof(struct)==rl.length - (struct is not defs.FILE_HEADER)
+        expected_size = ctypes.sizeof(struct) + (0 if struct is defs.FILE_HEADER else 1)
+        assert expected_size <= rl.length
+        if expected_size < rl.length:
+            print("WARNING: expected record_length %d for struct %r, but got %d in header"%(expected_size, struct, rl.length))
         offset += ctypes.sizeof(defs.RECORD_LENGTH)
 
     return tag, fh, rls, offset
 
-def read_record(buf, offset, tag2struct=defs.tag2struct):
+def read_record(buf, offset,
+                tag2rl={tag:ctypes.sizeof(struct) for tag, struct in defs.tag2struct.items()},
+                tag2struct=defs.tag2struct):
+
     tag = ord(buf[offset])
     if tag in tag2struct and tag!=32 and offset+1+ctypes.sizeof(tag2struct[tag])<=len(buf):
         struct = tag2struct[tag]
         assert struct != defs.FILE_HEADER
         record = struct.from_buffer_copy(buf, offset+1)
-        offset += 1 + ctypes.sizeof(struct)
-        return tag, record, offset
+        if 1+ctypes.sizeof(struct) < tag2rl[tag]:
+            extra = buf[offset+1+ctypes.sizeof(struct) : offset+tag2rl[tag]]
+        else:
+            extra = None
+        offset += tag2rl[tag]
+        return tag, record, offset, extra
     else:
         junkstart, junkend = offset, None
         while offset<len(buf):
@@ -66,7 +78,7 @@ def read_record(buf, offset, tag2struct=defs.tag2struct):
 
         struct = ReprBytes("SUSPECTED_JUNK", junkend-junkstart)
         record = struct.from_buffer_copy(buf, junkstart)
-        return None, record, offset
+        return None, record, offset, extra
 
 #####
 
@@ -85,8 +97,12 @@ if __name__=='__main__':
             print "-------- %02x %s\t[ %s ]" % (rl.tag, rl, defs.tag2struct[rl.tag].__name__)
 
         while offset < len(data):
-            tag, record, new_offset = read_record(data, offset)
-            print "%08x %s %s" % (offset, '__' if tag is None else ("%02x"%tag), record)
+            tag, record, new_offset, extra = read_record(data, offset, {rl.tag:rl.length for rl in rls})
+            if extra:
+                x = ReprBytes('EXTRA', len(extra)).from_buffer_copy(extra)
+            else:
+                x = ''
+            print "%08x %s %s %s" % (offset, '__' if tag is None else ("%02x"%tag), record, x)
             offset = new_offset
 
         assert offset==len(data)
